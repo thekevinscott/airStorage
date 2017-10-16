813f6dfc589a5271d77bb523fb558c70877be5ba
Kevin Scott <kevin@hitheory.com>
2017-10-16 16:22:53 -0400

    Add new build folder

--- dist/api/index.js
+++ dist/api/index.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _axios = require("axios");
 
 var _axios2 = _interopRequireDefault(_axios);
@@ -14,13 +12,11 @@ var _errors = require("../utils/errors");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-const root = "http://api.airstorage.io";
-// const root = "http://localhost:5050";
+const root = "https://api.airstorage.io";
 
-const api = options => {
-  return (0, _axios2.default)(_extends({}, options, {
-    url: `${root}${options.url}`
-  })).then(resp => {
+const api = (url, options) => {
+  const newUrl = `${root}${url}`;
+  return (0, _axios2.default)(newUrl, options).then(resp => {
     if (resp.data === undefined) {
       throw "Missing data response";
     }
